d02a0e61e0d4eb06f8134afa7213fa63afa6ad37
Samandar0021305 <tsamandardev89@gmail.com>
2024-01-06 21:17:22 +0900

    scss config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './plugins/router';
 
+import '@/assets/scss/index.scss';
+
 const app = createApp(App)
 
 app.use(createPinia())
