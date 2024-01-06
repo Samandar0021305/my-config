5305c71fab8d257cea64c355c1452da33a432362
Samandar0021305 <tsamandardev89@gmail.com>
2024-01-06 20:00:38 +0900

    my-config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw, RouterView } from 'vue-router';
+
+const routes:Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: () => import('@/layouts/default.vue'),
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    
-  ]
-})
-
-export default router
+  routes,
+});
+export default router;
