34ebe4d577f4603aecc59815b976e8001eb2c635
Samandar0021305 <tsamandardev89@gmail.com>
2024-01-07 19:34:01 +0900

    icon-config

--- src/utils/mixins.ts
+++ src/utils/mixins.ts
@@ -1,6 +1,5 @@
 
 
-
 export function MixinGetColor(color?: string): string {
     if (color) {
         return window.getComputedStyle(document.documentElement).getPropertyValue(`--color-${color}`)
